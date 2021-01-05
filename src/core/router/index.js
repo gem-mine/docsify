287698c525701b2ca44b2e5479ca50657da2dcfb
githoniel <githoniel88@gmail.com>
2021-01-05 22:17:17 +0800

    feat: 切换路由后关闭menu

--- src/core/router/index.js
+++ src/core/router/index.js
@@ -13,6 +13,7 @@ function updateRender(vm) {
   vm.router.normalize()
   vm.route = vm.router.parse()
   dom.body.setAttribute('data-page', vm.route.file)
+  dom.body.classList.remove('show-nav-mobile')
 }
 
 export function initRouter(vm) {
