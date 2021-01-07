005b0fc304c711309005ffe2575b508660e85fed
githoniel <githoniel88@gmail.com>
2021-01-07 18:04:46 +0800

    feat: support font-awesome

--- src/core/render/emojify.js
+++ src/core/render/emojify.js
@@ -4,9 +4,14 @@ function replace(m, $1) {
   return '<img class="emoji" src="https://github.githubassets.com/images/icons/emoji/' + $1 + '.png" alt="' + $1 + '" />'
 }
 
+function replaceFontAwesome(m, $1) {
+  return '<i class="' + $1 + '"></i>'
+}
+
 export function emojify(text) {
   return text
     .replace(/<(pre|template|code)[^>]*?>[\s\S]+?<\/(pre|template|code)>/g, m => m.replace(/:/g, '__colon__'))
     .replace(/:(\w+?):/ig, (inBrowser && window.emojify) || replace)
+    .replace(/:(fa[srldb](\s[\w-]+)\s[\w-]+?):/ig, replaceFontAwesome)
     .replace(/__colon__/g, ':')
 }
