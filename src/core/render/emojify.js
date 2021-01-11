9ed5940cdfec6919b4d8297d36954b866b8addff
githoniel <githoniel88@gmail.com>
2021-01-11 16:33:29 +0800

    fix: 修正fa正则

--- src/core/render/emojify.js
+++ src/core/render/emojify.js
@@ -12,6 +12,6 @@ export function emojify(text) {
   return text
     .replace(/<(pre|template|code)[^>]*?>[\s\S]+?<\/(pre|template|code)>/g, m => m.replace(/:/g, '__colon__'))
     .replace(/:(\w+?):/ig, (inBrowser && window.emojify) || replace)
-    .replace(/:(fa[srldb](\s[\w-])*\s[\w-]+):/ig, replaceFontAwesome)
+    .replace(/:(fa[srldb](\s[\w-]+)*\s[\w-]+):/ig, replaceFontAwesome)
     .replace(/__colon__/g, ':')
 }
